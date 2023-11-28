95da5e12b6e984d739c0a4aa866e4e908453a93d
skpntsv <n.skopintsev@g.nsu.ru>
2023-11-29 00:14:49 +0700

    it works!

---
+++ kinolist/src/main/java/ru/nsu/kinolist/bot/controller/KinoListBot.java
@@ -0,0 +1,49 @@
+package ru.nsu.kinolist.bot.controller;
+
+import lombok.extern.log4j.Log4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Log4j
+@Component
+public class KinoListBot extends TelegramLongPollingBot {
+
+    @Value("${bot.name}")
+    private String botName;
+
+    @Value("${bot.token}")
+    private String botToken;
+
+    @Override
+    public String getBotUsername() {
+        return botName;
+    }
+
+    @Override
+    public String getBotToken() {
+        return botToken;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        var originalMessage = update.getMessage();
+        System.out.println(originalMessage.getText());
+        sendMessage(originalMessage.getChatId(), "Hello my dear friend");
+    }
+
+    private void sendMessage(long chatId, String textToSend) {
+        SendMessage message = new SendMessage();
+        message.setChatId(String.valueOf(chatId));
+        message.setText(textToSend);
+
+        try {
+            execute(message);
+        } catch (TelegramApiException ex) {
+            log.error(ex);
+        }
+    }
+}
