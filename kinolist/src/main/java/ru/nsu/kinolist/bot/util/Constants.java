7a51d23475d95caf8b6b7b4414e456e6fde91d89
skpntsv <n.skopintsev@g.nsu.ru>
2023-12-10 16:36:01 +0700

    transfer some constants

--- kinolist/src/main/java/ru/nsu/kinolist/bot/util/Constants.java
+++ kinolist/src/main/java/ru/nsu/kinolist/bot/util/Constants.java
@@ -1,10 +1,25 @@
 package ru.nsu.kinolist.bot.util;
 
 public class Constants {
-    public static final String START_COMMAND_TEXT = "Начало работы";
-    public static final String HELP_COMMAND_TEXT = "Помощь";
-    public static final String SHOW_PLAYLISTS_COMMAND_TEXT = "Показать мои плейлисты";
-    public static final String MAIN_MENU = "Главное меню";
+    public static final String START_COMMAND = "/start";
+    public static final String START_COMMAND_DESCRIPTION = "Начало работы";
+
+    public static final String HELP_COMMAND = "/help";
+    public static final String HELP_COMMAND_DESCRIPTION = "Помощь";
+
+    public static final String SHOW_PLAYLISTS_COMMAND = "/show_playlists";
+    public static final String SHOW_PLAYLISTS_COMMAND_DESCRIPTION = "Показать мои плейлисты";
+
+    public static final String MENU_COMMAND = "/menu";
+    public static final String MAIN_MENU_COMMAND_TEXT = "Главное меню";
+
+
+    public static final String PLAYLISTS_COMMAND_TEXT = "Мои плейлисты";
+    public static final String ADD_WATCHEDLIST_COMMAND_TEXT = "Добавить в список просмотренных";
+    public static final String ADD_WISHLIST_COMMAND_TEXT = "Добавить в список желаемого";
+    public static final String ADD_TRACKEDLIST_COMMAND_TEXT = "Добавить в список отслеживаемого";
+    public static final String SEARCH_RANDOM_COMMAND_TEXT = "Найти случайный фильм";
+
     public static final String START_MESSAGE = """
                 **Добро пожаловать в бот, %s!**
                 
