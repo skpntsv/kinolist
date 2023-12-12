15bf7dd9ee0244fb59315e2e7cdadeec45a79866
skpntsv <n.skopintsev@g.nsu.ru>
2023-12-12 11:37:07 +0700

    make wishlist handlers

--- kinolist/src/main/java/ru/nsu/kinolist/bot/handlers/callbackquery/WishListQueryHandle.java
+++ kinolist/src/main/java/ru/nsu/kinolist/bot/handlers/callbackquery/WishListQueryHandle.java
@@ -1,16 +1,26 @@
 package ru.nsu.kinolist.bot.handlers.callbackquery;
 
+import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.methods.PartialBotApiMethod;
 import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
+import ru.nsu.kinolist.bot.service.WishlistService;
 
 import java.io.Serializable;
 import java.util.List;
 
+@Component
 public class WishListQueryHandle implements CallbackQueryHandler{
     private static final CallbackQueryType HANDLER_QUERY_TYPE = CallbackQueryType.WISHLIST;
+
+    private WishlistService wishlistService;
+
+    public WishListQueryHandle(WishlistService wishlistService) {
+        this.wishlistService = wishlistService;
+    }
+
     @Override
     public List<PartialBotApiMethod<? extends Serializable>> handleCallbackQuery(CallbackQuery callbackQuery) {
-        return null;
+        return wishlistService.getWishListMessage(callbackQuery.getMessage().getChatId(), callbackQuery.getMessage().getMessageId());
     }
 
     @Override
---
+++ kinolist/src/main/java/ru/nsu/kinolist/bot/service/WishlistService.java
@@ -0,0 +1,67 @@
+package ru.nsu.kinolist.bot.service;
+
+import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.meta.api.methods.ActionType;
+import org.telegram.telegrambots.meta.api.methods.PartialBotApiMethod;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageReplyMarkup;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import ru.nsu.kinolist.bot.handlers.callbackquery.CallbackQueryType;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+@Service
+public class WishlistService {
+    public List<PartialBotApiMethod<? extends Serializable>> getWishListMessage(Long chatId, Integer messageId) {
+        //sendAction(chatId, ActionType.TYPING);
+
+        EditMessageText editedMessage = new EditMessageText();
+        editedMessage.setChatId(chatId);
+        editedMessage.setMessageId(messageId);
+
+        editedMessage.setText(getWishList(chatId));
+
+        InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
+        List<List<InlineKeyboardButton>> rowsInline = new ArrayList<>();
+        // TODO сделать, константы, поставить нормальные callbacks
+        rowsInline.add(Collections.singletonList(MessagesService.getButton("Добавить фильм/сериал",
+                CallbackQueryType.WISHLIST.name() + "|" + CallbackQueryType.ADD.name())));
+        rowsInline.add(Collections.singletonList(MessagesService.getButton("Перенести фильм/сериал",
+                CallbackQueryType.WISHLIST.name() + "|" + CallbackQueryType.TRANSFER.name())));
+        rowsInline.add(Collections.singletonList(MessagesService.getButton("Удалить фильм/сериал из плейлиста",
+                CallbackQueryType.WISHLIST.name() + "|" + CallbackQueryType.REMOVE.name())));
+
+        inlineKeyboardMarkup.setKeyboard(rowsInline);
+
+        EditMessageReplyMarkup editMessageReplyMarkup = new EditMessageReplyMarkup();
+        editMessageReplyMarkup.setChatId(chatId);
+        editMessageReplyMarkup.setMessageId(messageId);
+        editMessageReplyMarkup.setReplyMarkup(inlineKeyboardMarkup);
+
+        List<PartialBotApiMethod<? extends Serializable>> messages = new ArrayList<>();
+
+        messages.add(editedMessage);
+        messages.add(editMessageReplyMarkup);
+        return messages;
+    }
+
+    public List<PartialBotApiMethod<? extends Serializable>> sendACKMessage(Long chatId, Integer messageId) {
+
+        return null;
+    }
+
+    private String getWishList(Long ChatId) {
+        // TODO нужно брать из БД список фильмов
+
+        return """
+                1. Матрица 4
+                2. Побег из Шоушенко
+                3. Рик и Морти (2013-н.в.)
+                """;
+    }
+}
