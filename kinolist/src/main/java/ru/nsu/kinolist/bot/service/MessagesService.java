7988e428308b31cedf7aeae5446319455ea57b08
skpntsv <n.skopintsev@g.nsu.ru>
2023-12-13 13:49:12 +0700

    new methods to create messages

--- kinolist/src/main/java/ru/nsu/kinolist/bot/service/MessagesService.java
+++ kinolist/src/main/java/ru/nsu/kinolist/bot/service/MessagesService.java
@@ -1,14 +1,33 @@
 package ru.nsu.kinolist.bot.service;
 
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 
 public class MessagesService {
     public static InlineKeyboardButton getButton(String nameButton, String callBackData){
-        var button = new InlineKeyboardButton();
+        InlineKeyboardButton button = new InlineKeyboardButton();
 
         button.setText(nameButton);
         button.setCallbackData(callBackData);
 
         return button;
+    }
+
+    public static SendMessage createMessageTemplate(String chatId, String text) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(chatId);
+        sendMessage.enableMarkdown(true);
+        sendMessage.setText(text);
+
+        return sendMessage;
+    }
+
+    public static SendMessage createMessageTemplate(Long chatId, String text) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(chatId);
+        sendMessage.enableMarkdown(true);
+        sendMessage.setText(text);
+
+        return sendMessage;
     }
 }
