6ff1a0c451cf23d0fabf2d57fe167ca44b7fc53c
skpntsv <n.skopintsev@g.nsu.ru>
2023-12-14 00:59:14 +0700

    refactoring

--- kinolist/src/main/java/ru/nsu/kinolist/bot/service/MessagesService.java
+++ kinolist/src/main/java/ru/nsu/kinolist/bot/service/MessagesService.java
@@ -3,9 +3,13 @@ package ru.nsu.kinolist.bot.service;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+import ru.nsu.kinolist.bot.handlers.callbackquery.CallbackQueryType;
+import ru.nsu.kinolist.bot.handlers.callbackquery.ParseQueryData;
+import ru.nsu.kinolist.database.entities.Film;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class MessagesService {
     public static InlineKeyboardButton getButton(String nameButton, String callBackData){
@@ -35,16 +39,23 @@ public class MessagesService {
         return sendMessage;
     }
 
-    public static InlineKeyboardMarkup createYesOrNoButton(String callBackDataOnYes, String callBackDataOnNo) {
+    public static InlineKeyboardMarkup createYesOrNoButton(CallbackQueryType playlist, CallbackQueryType operation, int filmId) {
         InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
         List<List<InlineKeyboardButton>> rowsInline = new ArrayList<>();
 
         // Добавляем кнопку "Да"
+        String callBackDataOnYes = ParseQueryData.createCallbackData(playlist.name(),
+                operation.name(),
+                CallbackQueryType.YES.name(),
+                String.valueOf(filmId));
         List<InlineKeyboardButton> rowInlineYes = new ArrayList<>();
         rowInlineYes.add(getButton("Да", callBackDataOnYes));
         rowsInline.add(rowInlineYes);
 
         // Добавляем кнопку "Нет"
+        String callBackDataOnNo = ParseQueryData.createCallbackData(playlist.name(),
+                operation.name(),
+                CallbackQueryType.NO.name());
         List<InlineKeyboardButton> rowInlineNo = new ArrayList<>();
         rowInlineYes.add(getButton("Нет", callBackDataOnNo));
         rowsInline.add(rowInlineNo);
@@ -52,5 +63,9 @@ public class MessagesService {
         inlineKeyboardMarkup.setKeyboard(rowsInline);
 
         return inlineKeyboardMarkup;
+    }
+
+    public static SendMessage errorMessage(Long chatId) {
+        return createMessageTemplate(chatId, "Что-то пошло не так, попробуйте ещё раз");
     }
 }
