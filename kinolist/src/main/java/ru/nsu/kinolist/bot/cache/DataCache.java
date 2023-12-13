a128763b5bc5e67312805f76b014463d12a77cfb
skpntsv <n.skopintsev@g.nsu.ru>
2023-12-14 00:57:42 +0700

    now we save list of films

--- kinolist/src/main/java/ru/nsu/kinolist/bot/cache/DataCache.java
+++ kinolist/src/main/java/ru/nsu/kinolist/bot/cache/DataCache.java
@@ -1,10 +1,6 @@
 package ru.nsu.kinolist.bot.cache;
 
-import ru.nsu.kinolist.bot.Film;
 import ru.nsu.kinolist.bot.util.BotState;
-
-import java.util.List;
-import java.util.Optional;
 
 /**
  * interface for saving statement for each user
--- kinolist/src/main/java/ru/nsu/kinolist/bot/cache/UserDataCache.java
+++ kinolist/src/main/java/ru/nsu/kinolist/bot/cache/UserDataCache.java
@@ -1,8 +1,8 @@
 package ru.nsu.kinolist.bot.cache;
 
 import org.springframework.stereotype.Service;
-import ru.nsu.kinolist.bot.Film;
 import ru.nsu.kinolist.bot.util.BotState;
+import ru.nsu.kinolist.database.entities.Film;
 
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
@@ -10,6 +10,7 @@ import java.util.concurrent.ConcurrentHashMap;
 @Service
 public class UserDataCache implements DataCache {
     private final Map<Long, BotState> usersBotStates = new ConcurrentHashMap<>();
+    private final Map<Long, List<Film>> movieListOfUser = new ConcurrentHashMap<>();
 
     @Override
     public void setUsersCurrentBotState(Long chatId, BotState botState) {
@@ -28,5 +29,17 @@ public class UserDataCache implements DataCache {
 
     public void removeUsersCache(Long chatId) {
         usersBotStates.remove(chatId);
+    }
+
+    public void setCurrentMovieListOfUser(Long chatId, List<Film> movies) {
+        movieListOfUser.put(chatId, movies);
+    }
+
+    public List<Film> getCurrentMovieListOfUser(Long chatId) {
+        return movieListOfUser.get(chatId);
+    }
+
+    public void removeCurrentMovieListOfUser(Long chatId) {
+        movieListOfUser.remove(chatId);
     }
 }
