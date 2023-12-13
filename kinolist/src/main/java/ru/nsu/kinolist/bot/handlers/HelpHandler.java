50d8dd41be5ab361c9a0bc2316e7d21571544ea7
skpntsv <n.skopintsev@g.nsu.ru>
2023-12-13 20:02:21 +0700

    add help handler

---
+++ kinolist/src/main/java/ru/nsu/kinolist/bot/handlers/HelpHandler.java
@@ -0,0 +1,25 @@
+package ru.nsu.kinolist.bot.handlers;
+
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.methods.PartialBotApiMethod;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import ru.nsu.kinolist.bot.service.MessagesService;
+import ru.nsu.kinolist.bot.util.BotState;
+import ru.nsu.kinolist.bot.util.Constants;
+
+import java.io.Serializable;
+import java.util.List;
+
+@Component
+public class HelpHandler implements InputMessageHandler {
+
+    @Override
+    public List<PartialBotApiMethod<? extends Serializable>> handleMessage(Message message) {
+        return List.of(MessagesService.createMessageTemplate(message.getChatId(), Constants.HELP_MESSAGE));
+    }
+
+    @Override
+    public BotState getHandlerName() {
+        return BotState.SHOW_HELP;
+    }
+}
